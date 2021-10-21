be9b0a17ee7b4f05caf3a5d77cdd34376e5d58e0
Your Name <you@example.com>
2021-10-20 21:45:50 -0600

    rguzma

---
+++ HumanTest.java
@@ -0,0 +1,14 @@
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Human h = new Human(); /*Function in case of set a constructor*/
+		Human o = new Human();
+		h.attack (o);
+		System.out.println(o.health);
+
+	}
+
+}
