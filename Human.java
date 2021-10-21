70309d70c2c2ab28fe13f339f45a080571a25ce2
Your Name <you@example.com>
2021-10-21 13:35:39 -0600

    rguzma

---
+++ Human.java
@@ -0,0 +1,12 @@
+
+public class Human {
+
+    public int strength = 3;
+    public int intelligence = 3;
+    public int stealth = 3;
+    public int health = 100;
+    public int attack (Human oponent ) {
+        oponent.health -= this.strength;
+        return oponent.health;
+    }
+}
